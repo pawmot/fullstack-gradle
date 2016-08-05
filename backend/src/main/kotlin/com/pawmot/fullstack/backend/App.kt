f18b45cba2aa9990b3ea8044abeb3c2e08a6267b
Paweł Motyl <pawmot@gmail.com>
2016-08-05 21:04:03 +0200

    Application seed
    Created a seed of Spring app in Kotlin.

---
+++ backend/src/main/kotlin/com/pawmot/fullstack/backend/App.kt
@@ -0,0 +1,12 @@
+package com.pawmot.fullstack.backend;
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class App {
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(App::class.java, *args)
+}
